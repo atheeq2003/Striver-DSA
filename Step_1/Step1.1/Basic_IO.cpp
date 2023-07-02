0a1a6f9f97901dd01cccf2da3c0a13814f66e8ac
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-02 20:52:50 +0530

    Basic IO

---
+++ Step_1/Step1.1/Basic_IO.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int x, y;
+  cin >> x >> y;
+  cout << "Atheequr = " << x << endl;
+  cout << "Rahaman = " << y << endl;
+  return 0;
+}
