ed0a5587e77df849810a14036bad71a839431d8e
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-03 14:54:42 +0530

    Arrays

---
+++ Step_1/Step1.1/Arrays.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[4];
+  cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+  cout << arr[4] << endl; 
+  int a[3][5];
+  a[1][3] = 37;
+  cout << a[1][3] << endl;
+}
