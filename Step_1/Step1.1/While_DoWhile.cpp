088b2813ea9c29c762d96edd33c3ebbe54f90b93
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-03 15:31:13 +0530

    While-DoWhile

---
+++ Step_1/Step1.1/While_DoWhile.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int i=0;
+  while(i<5)
+  {
+    cout << "Atheeq " << i << endl;
+    i++;
+  }
+  cout << i << endl;
+  int j=3;
+  do
+  {
+    cout << "Rahaman " << j << endl;
+    j++;
+  }while(j<5);
+  cout << j << endl;
+}
