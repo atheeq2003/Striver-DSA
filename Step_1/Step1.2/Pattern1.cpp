9f3d4c08e6abcad263540f6e993798dd7083ef1a
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-06 13:15:52 +0530

    Pattern-1

---
+++ Step_1/Step1.2/Pattern1.cpp
@@ -0,0 +1,21 @@
+/* Pattern 1
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * *
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  for(int i = 0; i < 5; i++)
+  {
+    for(int j = 0;j < 5;j++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
