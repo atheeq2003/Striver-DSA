82616c532281411df82d9264f9d825e5047b4769
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-09 22:46:18 +0530

    Patter-15

---
+++ Step_1/Step1.2/Pattern15.cpp
@@ -0,0 +1,29 @@
+/*
+  Pattern 15
+  A B C D E 
+  A B C D 
+  A B C
+  A B
+  A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void print15(int n)
+{
+  for(int i=1;i<=n;i++)
+  {
+    for(char j='A';j<='A'+n-i-1+1;j++)
+    {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+}           
+
+int main()
+{
+  int n;
+  cin >> n;
+  print15(n);
+  return 0;
+}
