0a5e5a6fd551ce83aaafde30e320be6698e5c63c
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-07-10 23:29:58 +0530

    Pattern-17

---
+++ Step_1/Step1.2/Pattern17.cpp
@@ -0,0 +1,39 @@
+/*
+  Pattern 17
+     A     
+    ABA    
+   ABCBA   
+  ABCDCBA  
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void print17(int n)
+{
+  for(int i=0;i<n;i++)
+  {
+    for(int j=0;j<n-i-1;j++)
+    {
+      cout<<" ";
+    }
+    char ch = 'A';
+    int breakpoint = (2*i+1)/2;
+    for(int j=1;j<=2*i+1;j++)
+    {
+      cout<<ch;
+      if(j <= breakpoint) ch++;
+      else ch--;
+    }
+    for(int j=0;j<n-i-1;j++)
+    {
+      cout<<" ";
+    }
+    cout << endl;
+  }
+}         
+int main()
+{
+  int n;
+  cin >> n;
+  print17(n);
+  return 0;
+}
